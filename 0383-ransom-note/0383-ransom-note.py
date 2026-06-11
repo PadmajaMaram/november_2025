0bb6cd788fee370e3d5041758ae805b21267bc2f
PadmajaMaram <padhumaram09@gmail.com>
2026-06-11 18:58:23 +0530

    Time: 23 ms (85.98%), Space: 12.8 MB (20.51%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        dict1={}
+        dict2={}
+        for ch in magazine:
+            if ch in dict1:
+                dict1[ch]+=1
+            else:
+                dict1[ch]=1
+        for ch in ransomNote:
+            if ch in dict2:
+                dict2[ch]+=1
+            else:
+                dict2[ch]=1
+        for ch in dict2:
+            if ch not in dict1 or dict2[ch]>dict1[ch]:
+                return False
+        return True
+                
+
+        
