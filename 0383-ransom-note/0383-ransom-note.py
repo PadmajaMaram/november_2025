ec1ae5a8f14189253c7d6f76ea468fed79f5e9d1
PadmajaMaram <padhumaram09@gmail.com>
2026-07-05 12:11:45 +0530

    Time: 24 ms (42.35%), Space: 19.8 MB (8.03%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,26 +1,19 @@
-class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
-        """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
-        """
-        dict1={}
-        dict2={}
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hashmap={}
         for ch in magazine:
-            if ch in dict1:
-                dict1[ch]+=1
+            if ch not in hashmap:
+                hashmap[ch]=1
             else:
-                dict1[ch]=1
+                hashmap[ch]+=1
         for ch in ransomNote:
-            if ch in dict2:
-                dict2[ch]+=1
+            if ch in hashmap:
+                hashmap[ch]-=1
+                if hashmap[ch]<0:
+                    return False
             else:
-                dict2[ch]=1
-        for ch in dict2:
-            if ch not in dict1 or dict2[ch]>dict1[ch]:
                 return False
         return True
-                
+
 
         
